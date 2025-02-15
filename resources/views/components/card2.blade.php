199b857b3e8f93e709a11e4e05623272a722989f
Ardi Hergustiyan <hergustiyan@gmail.com>
2025-02-15 14:42:01 +0700

    mengatur tampilan pada mobile agar lebih baik

--- resources/views/components/card2.blade.php
+++ resources/views/components/card2.blade.php
@@ -1,25 +1,27 @@
-<a href="{{ route('product.show', $product->slug) }}" class="swiper-slide bg-white hover:cursor-pointer rounded-lg overflow-hidden w-max border border-pink-200 flex items-center p-6 space-x-4">
-  @if ($product->productVariant->isNotEmpty())
-      <img 
-          src="{{ asset('storage/' . (is_array($product->productVariant->first()->image) ? $product->productVariant->first()->image[0] : $product->productVariant->first()->image)) }}" 
-          alt="{{ $product->name }}" 
-          class="h-16 w-16 rounded-lg object-cover"
-      >
-  @else
-      <img 
-          src="{{ asset('storage/' . (is_array($product->image) ? $product->image[0] : $product->image)) }}" 
-          alt="{{ $product->name }}" 
-          class="h-16 w-16 rounded-lg object-cover"
-      >
-  @endif
-  <div>
-    <h3 class="text-gray-700 font-semibold text-lg">{{ count(explode(' ', $product->name)) > 4   
-      ? implode(' ', array_slice(explode(' ', $product->name), 0, 4)) . '...' 
-      : $product->name }}</h3>
-    <p class="text-gray-600 text-sm">
-      {{ count(explode(' ', $product->description)) > 7 
-          ? implode(' ', array_slice(explode(' ', $product->description), 0, 7)) . '...' 
-          : $product->description }}
-  </p>
-  </div>
+<a href="{{ route('product.show', $product->slug) }}" 
+  class="swiper-slide bg-white hover:cursor-pointer rounded-lg overflow-hidden w-full sm:w-max border border-pink-200 flex items-center p-4 sm:p-6 space-x-3 sm:space-x-4 h-24 sm:h-32">
+ <!-- Gambar Produk -->
+ @if ($product->productVariant->isNotEmpty())
+   <img 
+     src="{{ asset('storage/' . (is_array($product->productVariant->first()->image) ? $product->productVariant->first()->image[0] : $product->productVariant->first()->image)) }}" 
+     alt="{{ $product->name }}" 
+     class="h-12 w-12 sm:h-16 sm:w-16 rounded-lg object-cover flex-shrink-0"
+   >
+ @else
+   <img 
+     src="{{ asset('storage/' . (is_array($product->image) ? $product->image[0] : $product->image)) }}" 
+     alt="{{ $product->name }}" 
+     class="h-12 w-12 sm:h-16 sm:w-16 rounded-lg object-cover flex-shrink-0"
+   >
+ @endif
+
+ <!-- Konten Teks -->
+ <div class="flex-1 overflow-hidden">
+   <h3 class="text-gray-700 font-semibold text-base sm:text-lg line-clamp-2">
+     {{ $product->name }}
+   </h3>
+   <p class="text-gray-600 text-xs sm:text-sm line-clamp-2 mt-1">
+     {{ $product->description }}
+   </p>
+ </div>
 </a>
--- resources/views/components/features.blade.php
+++ resources/views/components/features.blade.php
@@ -1,82 +1,88 @@
-{{-- features --}}
-<div class="container mx-auto px-4 p-6">
-    <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-6">
+<div class="container mx-auto px-4 py-6">
+  <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4 sm:gap-6">
+      <!-- Card 1 -->
       <div class="card flex items-start p-4 border-0">
-        <div class="text-dark w-8 h-8 flex-shrink-0">
-          <!-- Icon -->
-          <svg class="w-8 h-8" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M21.5 15a3 3 0 0 0-1.9-2.78l1.87-7a1 1 0 0 0-.18-.87A1 1 0 0 0 20.5 4H6.8l-.33-1.26A1 1 0 0 0 5.5 2h-2v2h1.23l2.48 9.26a1 1 0 0 0 1 .74H18.5a1 1 0 0 1 0 2h-13a1 1 0 0 0 0 2h1.18a3 3 0 1 0 5.64 0h2.36a3 3 0 1 0 5.82 1a2.94 2.94 0 0 0-.4-1.47A3 3 0 0 0 21.5 15Zm-3.91-3H9L7.34 6H19.2ZM9.5 20a1 1 0 1 1 1-1a1 1 0 0 1-1 1Zm8 0a1 1 0 1 1 1-1a1 1 0 0 1-1 1Z"
-            ></path>
-          </svg>
-        </div>
-        <div class="ml-4">
-          <h5 class="font-semibold text-lg">Free delivery</h5>
-          <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
-        </div>
+          <div class="text-dark w-8 h-8 flex-shrink-0">
+              <!-- Icon -->
+              <svg class="w-8 h-8" viewBox="0 0 24 24">
+                  <path
+                      fill="currentColor"
+                      d="M21.5 15a3 3 0 0 0-1.9-2.78l1.87-7a1 1 0 0 0-.18-.87A1 1 0 0 0 20.5 4H6.8l-.33-1.26A1 1 0 0 0 5.5 2h-2v2h1.23l2.48 9.26a1 1 0 0 0 1 .74H18.5a1 1 0 0 1 0 2h-13a1 1 0 0 0 0 2h1.18a3 3 0 1 0 5.64 0h2.36a3 3 0 1 0 5.82 1a2.94 2.94 0 0 0-.4-1.47A3 3 0 0 0 21.5 15Zm-3.91-3H9L7.34 6H19.2ZM9.5 20a1 1 0 1 1 1-1a1 1 0 0 1-1 1Zm8 0a1 1 0 1 1 1-1a1 1 0 0 1-1 1Z"
+                  ></path>
+              </svg>
+          </div>
+          <div class="ml-4">
+              <h5 class="font-semibold text-base sm:text-lg">Free delivery</h5>
+              <p class="text-gray-500 text-xs sm:text-sm">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
+          </div>
       </div>
-      <!-- Repeat the above block for each card -->
+
+      <!-- Card 2 -->
       <div class="card flex items-start p-4 border-0">
-        <div class="text-dark w-8 h-8 flex-shrink-0">
-          <!-- Icon -->
-          <svg class="w-8 h-8" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M19.63 3.65a1 1 0 0 0-.84-.2a8 8 0 0 1-6.22-1.27a1 1 0 0 0-1.14 0a8 8 0 0 1-6.22 1.27a1 1 0 0 0-.84.2a1 1 0 0 0-.37.78v7.45a9 9 0 0 0 3.77 7.33l3.65 2.6a1 1 0 0 0 1.16 0l3.65-2.6A9 9 0 0 0 20 11.88V4.43a1 1 0 0 0-.37-.78ZM18 11.88a7 7 0 0 1-2.93 5.7L12 19.77l-3.07-2.19A7 7 0 0 1 6 11.88v-6.3a10 10 0 0 0 6-1.39a10 10 0 0 0 6 1.39Zm-4.46-2.29l-2.69 2.7l-.89-.9a1 1 0 0 0-1.42 1.42l1.6 1.6a1 1 0 0 0 1.42 0L15 11a1 1 0 0 0-1.42-1.42Z"
-            ></path>
-          </svg>
-        </div>
-        <div class="ml-4">
-          <h5 class="font-semibold text-lg">100% secure payment</h5>
-          <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
-        </div>
+          <div class="text-dark w-8 h-8 flex-shrink-0">
+              <!-- Icon -->
+              <svg class="w-8 h-8" viewBox="0 0 24 24">
+                  <path
+                      fill="currentColor"
+                      d="M19.63 3.65a1 1 0 0 0-.84-.2a8 8 0 0 1-6.22-1.27a1 1 0 0 0-1.14 0a8 8 0 0 1-6.22 1.27a1 1 0 0 0-.84.2a1 1 0 0 0-.37.78v7.45a9 9 0 0 0 3.77 7.33l3.65 2.6a1 1 0 0 0 1.16 0l3.65-2.6A9 9 0 0 0 20 11.88V4.43a1 1 0 0 0-.37-.78ZM18 11.88a7 7 0 0 1-2.93 5.7L12 19.77l-3.07-2.19A7 7 0 0 1 6 11.88v-6.3a10 10 0 0 0 6-1.39a10 10 0 0 0 6 1.39Zm-4.46-2.29l-2.69 2.7l-.89-.9a1 1 0 0 0-1.42 1.42l1.6 1.6a1 1 0 0 0 1.42 0L15 11a1 1 0 0 0-1.42-1.42Z"
+                  ></path>
+              </svg>
+          </div>
+          <div class="ml-4">
+              <h5 class="font-semibold text-base sm:text-lg">100% secure payment</h5>
+              <p class="text-gray-500 text-xs sm:text-sm">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
+          </div>
       </div>
+
+      <!-- Card 3 -->
       <div class="card flex items-start p-4 border-0">
-        <div class="text-dark w-8 h-8 flex-shrink-0">
-          <!-- Icon -->
-          <svg class="w-8 h-8" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M19.63 3.65a1 1 0 0 0-.84-.2a8 8 0 0 1-6.22-1.27a1 1 0 0 0-1.14 0a8 8 0 0 1-6.22 1.27a1 1 0 0 0-.84.2a1 1 0 0 0-.37.78v7.45a9 9 0 0 0 3.77 7.33l3.65 2.6a1 1 0 0 0 1.16 0l3.65-2.6A9 9 0 0 0 20 11.88V4.43a1 1 0 0 0-.37-.78ZM18 11.88a7 7 0 0 1-2.93 5.7L12 19.77l-3.07-2.19A7 7 0 0 1 6 11.88v-6.3a10 10 0 0 0 6-1.39a10 10 0 0 0 6 1.39Zm-4.46-2.29l-2.69 2.7l-.89-.9a1 1 0 0 0-1.42 1.42l1.6 1.6a1 1 0 0 0 1.42 0L15 11a1 1 0 0 0-1.42-1.42Z"
-            ></path>
-          </svg>
-        </div>
-        <div class="ml-4">
-          <h5 class="font-semibold text-lg">100% secure payment</h5>
-          <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
-        </div>
+          <div class="text-dark w-8 h-8 flex-shrink-0">
+              <!-- Icon -->
+              <svg class="w-8 h-8" viewBox="0 0 24 24">
+                  <path
+                      fill="currentColor"
+                      d="M19.63 3.65a1 1 0 0 0-.84-.2a8 8 0 0 1-6.22-1.27a1 1 0 0 0-1.14 0a8 8 0 0 1-6.22 1.27a1 1 0 0 0-.84.2a1 1 0 0 0-.37.78v7.45a9 9 0 0 0 3.77 7.33l3.65 2.6a1 1 0 0 0 1.16 0l3.65-2.6A9 9 0 0 0 20 11.88V4.43a1 1 0 0 0-.37-.78ZM18 11.88a7 7 0 0 1-2.93 5.7L12 19.77l-3.07-2.19A7 7 0 0 1 6 11.88v-6.3a10 10 0 0 0 6-1.39a10 10 0 0 0 6 1.39Zm-4.46-2.29l-2.69 2.7l-.89-.9a1 1 0 0 0-1.42 1.42l1.6 1.6a1 1 0 0 0 1.42 0L15 11a1 1 0 0 0-1.42-1.42Z"
+                  ></path>
+              </svg>
+          </div>
+          <div class="ml-4">
+              <h5 class="font-semibold text-base sm:text-lg">Quality guarantee</h5>
+              <p class="text-gray-500 text-xs sm:text-sm">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
+          </div>
       </div>
+
+      <!-- Card 4 -->
       <div class="card flex items-start p-4 border-0">
-        <div class="text-dark w-8 h-8 flex-shrink-0">
-          <!-- Icon -->
-          <svg class="w-8 h-8" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M19.63 3.65a1 1 0 0 0-.84-.2a8 8 0 0 1-6.22-1.27a1 1 0 0 0-1.14 0a8 8 0 0 1-6.22 1.27a1 1 0 0 0-.84.2a1 1 0 0 0-.37.78v7.45a9 9 0 0 0 3.77 7.33l3.65 2.6a1 1 0 0 0 1.16 0l3.65-2.6A9 9 0 0 0 20 11.88V4.43a1 1 0 0 0-.37-.78ZM18 11.88a7 7 0 0 1-2.93 5.7L12 19.77l-3.07-2.19A7 7 0 0 1 6 11.88v-6.3a10 10 0 0 0 6-1.39a10 10 0 0 0 6 1.39Zm-4.46-2.29l-2.69 2.7l-.89-.9a1 1 0 0 0-1.42 1.42l1.6 1.6a1 1 0 0 0 1.42 0L15 11a1 1 0 0 0-1.42-1.42Z"
-            ></path>
-          </svg>
-        </div>
-        <div class="ml-4">
-          <h5 class="font-semibold text-lg">100% secure payment</h5>
-          <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
-        </div>
+          <div class="text-dark w-8 h-8 flex-shrink-0">
+              <!-- Icon -->
+              <svg class="w-8 h-8" viewBox="0 0 24 24">
+                  <path
+                      fill="currentColor"
+                      d="M19.63 3.65a1 1 0 0 0-.84-.2a8 8 0 0 1-6.22-1.27a1 1 0 0 0-1.14 0a8 8 0 0 1-6.22 1.27a1 1 0 0 0-.84.2a1 1 0 0 0-.37.78v7.45a9 9 0 0 0 3.77 7.33l3.65 2.6a1 1 0 0 0 1.16 0l3.65-2.6A9 9 0 0 0 20 11.88V4.43a1 1 0 0 0-.37-.78ZM18 11.88a7 7 0 0 1-2.93 5.7L12 19.77l-3.07-2.19A7 7 0 0 1 6 11.88v-6.3a10 10 0 0 0 6-1.39a10 10 0 0 0 6 1.39Zm-4.46-2.29l-2.69 2.7l-.89-.9a1 1 0 0 0-1.42 1.42l1.6 1.6a1 1 0 0 0 1.42 0L15 11a1 1 0 0 0-1.42-1.42Z"
+                  ></path>
+              </svg>
+          </div>
+          <div class="ml-4">
+              <h5 class="font-semibold text-base sm:text-lg">24/7 support</h5>
+              <p class="text-gray-500 text-xs sm:text-sm">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
+          </div>
       </div>
+
+      <!-- Card 5 -->
       <div class="card flex items-start p-4 border-0">
-        <div class="text-dark w-8 h-8 flex-shrink-0">
-          <!-- Icon -->
-          <svg class="w-8 h-8" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M19.63 3.65a1 1 0 0 0-.84-.2a8 8 0 0 1-6.22-1.27a1 1 0 0 0-1.14 0a8 8 0 0 1-6.22 1.27a1 1 0 0 0-.84.2a1 1 0 0 0-.37.78v7.45a9 9 0 0 0 3.77 7.33l3.65 2.6a1 1 0 0 0 1.16 0l3.65-2.6A9 9 0 0 0 20 11.88V4.43a1 1 0 0 0-.37-.78ZM18 11.88a7 7 0 0 1-2.93 5.7L12 19.77l-3.07-2.19A7 7 0 0 1 6 11.88v-6.3a10 10 0 0 0 6-1.39a10 10 0 0 0 6 1.39Zm-4.46-2.29l-2.69 2.7l-.89-.9a1 1 0 0 0-1.42 1.42l1.6 1.6a1 1 0 0 0 1.42 0L15 11a1 1 0 0 0-1.42-1.42Z"
-            ></path>
-          </svg>
-        </div>
-        <div class="ml-4">
-          <h5 class="font-semibold text-lg">100% secure payment</h5>
-          <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
-        </div>
+          <div class="text-dark w-8 h-8 flex-shrink-0">
+              <!-- Icon -->
+              <svg class="w-8 h-8" viewBox="0 0 24 24">
+                  <path
+                      fill="currentColor"
+                      d="M19.63 3.65a1 1 0 0 0-.84-.2a8 8 0 0 1-6.22-1.27a1 1 0 0 0-1.14 0a8 8 0 0 1-6.22 1.27a1 1 0 0 0-.84.2a1 1 0 0 0-.37.78v7.45a9 9 0 0 0 3.77 7.33l3.65 2.6a1 1 0 0 0 1.16 0l3.65-2.6A9 9 0 0 0 20 11.88V4.43a1 1 0 0 0-.37-.78ZM18 11.88a7 7 0 0 1-2.93 5.7L12 19.77l-3.07-2.19A7 7 0 0 1 6 11.88v-6.3a10 10 0 0 0 6-1.39a10 10 0 0 0 6 1.39Zm-4.46-2.29l-2.69 2.7l-.89-.9a1 1 0 0 0-1.42 1.42l1.6 1.6a1 1 0 0 0 1.42 0L15 11a1 1 0 0 0-1.42-1.42Z"
+                  ></path>
+              </svg>
+          </div>
+          <div class="ml-4">
+              <h5 class="font-semibold text-base sm:text-lg">Easy returns</h5>
+              <p class="text-gray-500 text-xs sm:text-sm">Lorem ipsum dolor sit amet, consectetur adipi elit.</p>
+          </div>
       </div>
-    </div>
   </div>
-  {{-- End of Features --}}
+</div>
